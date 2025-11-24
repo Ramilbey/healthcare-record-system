240ac7337d4d8558ab7ce37d8a9fdc1d2ebfb4d0
mrsand <ramilbey505711503@gamil.com>
2025-11-24 10:33:17 +0800

    creating menu

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<< "Menu"<< endl;
+
+    cout << "generate records"<< endl;
+    cout << "show unsorted Radiology records" << endl;
+
+    cout << "go back"<< endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
